534a592f1d00b108f6d70d07e40f7a87a7b2d184
lucaspett <lucaspettinato2018@u.northwestern.edu>
2015-07-29 11:52:00 +0200

    Added additional seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,8 @@ Photo.delete_all
 
 puts "Creating photos..."
 
-Photo.create! public: true, caption: "Me in all my glory", image: "http://nyulocal.com/wp-content/uploads/2009/04/britney-spears-ugly-glasses-stoned.jpg"
+Photo.create! public: true, caption: "It's Britney, Bitch", image: "http://nyulocal.com/wp-content/uploads/2009/04/britney-spears-ugly-glasses-stoned.jpg"
+
+Photo.create! public: true, caption: "Tbt to when I was an ostrich", image: "http://ak-hdl.buzzfed.com/static/enhanced/web05/2012/5/1/21/enhanced-buzz-10015-1335923979-10.jpg"
+
+Photo.create! public: true, caption: "That moment when I dont win an oscar, again", image: "http://images6.fanpop.com/image/photos/34400000/Celebs-With-No-Teeth-funny-celebrity-moments-34438202-625-784.jpg"
